ccedd0ae37dec45e6359c35175175d83206bbe05
Whj <447639236@qq.com>
2019-12-29 17:58:48 +0800

    实现学习者管理页面

---
+++ src/main/java/com/hit/lpm/management/dao/RecTestMapper.java
@@ -0,0 +1,8 @@
+package com.hit.lpm.management.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.hit.lpm.management.model.RecTest;
+
+public interface RecTestMapper extends BaseMapper<RecTest> {
+
+}
